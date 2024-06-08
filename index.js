fe8469f16b2d6177a9376e0171e1bfe04752023e
Phuong1809 <21120534@student.hcmus.edu.vn>
2024-06-08 07:47:23 +0700

    change shce2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 
 
-cron.schedule('45 7 * * *', async () => {
+cron.schedule('50 7 * * *', async () => {
     console.log('Sending daily weather updates');
     subscribers= await db.Subscribers.findAll()
     console.log("subcribers ", subscribers)
